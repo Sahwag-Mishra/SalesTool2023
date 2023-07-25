1d1e1e2e6725e6610b0036f2907c0d65c2358c41
sahwagmishra <sahwagmishra@EZDI-LAP0086>
2023-07-25 18:15:33 +0530

    Initial commit of project

---
+++ SalesTool2018/src/SalesData.java
@@ -0,0 +1,17 @@
+
+public class SalesData 
+{
+	
+	public static void displayGreeting()
+	{
+		System.out.println("Hello happy sales people !");
+	    System.out.println("This app shows sales data");
+	}
+	public static void main(String[]args)
+	{
+		SalesData s1 = new SalesData();
+		displayGreeting();
+		         
+	}
+
+}
